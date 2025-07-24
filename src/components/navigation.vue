dccef5a257dab2d9f01284d602c10ca380a36a80
MumuYme <manacharin@gmail.com>
2025-07-25 05:24:06 +0900

    ナビバーの修正

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -7,25 +7,28 @@
         <img src="/logos/logo-dark.svg" class="h-8" alt="Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">TDU FI OC</span>
     </router-link>
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+    <button  @click="isMenuOpen = !isMenuOpen" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+    <div :class="['md:block md:w-auto w-full', isMenuOpen ? 'block' : 'hidden']" id="navbar-default">
       <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <router-link to="/" class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</router-link>
+          <router-link to="/" @click="closeMenu" 
+          class="block py-2 px-3 rounded-sm md:bg-transparent md:p-0  " 
+          active-class="text-blue font-bold" exact-active-class="text-blue-700 font-bold">Home</router-link>
         </li>
         <li>
-          <router-link to="/department" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">学科紹介</router-link>
+          <router-link to="/department" @click="closeMenu"  class="block py-2 px-3 rounded-sm md:bg-transparent md:p-0 " 
+          active-class="text-blue font-bold" exact-active-class="text-blue-700 font-bold">学科紹介</router-link>
         </li>
         <li>
-          <router-link :to="{ path: '/', hash: '#lab' }"  class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">研究室紹介</router-link>
+          <router-link :to="{ path: '/', hash: '#lab' }" @click="closeMenu" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">研究室紹介</router-link>
         </li>
         <li>
-          <router-link :to="{ path: '/', hash: '#schedule' }" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">スケジュール</router-link>
+          <router-link :to="{ path: '/', hash: '#schedule' }" @click="closeMenu" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">スケジュール</router-link>
         </li>
         <li>
           <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">地図</a>
@@ -44,5 +47,12 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+const isMenuOpen = ref(false)
+
+// ページ押されたら閉じる
+function closeMenu() {
+  isMenuOpen.value = false
+}
 </script>
