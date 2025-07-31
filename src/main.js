ea33239cb19af9a3fb767a5e6f40c63989971de8
MumuYme <manacharin@gmail.com>
2025-08-01 01:04:25 +0900

    アナリティクス調整

--- src/main.js
+++ src/main.js
@@ -1,21 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router';
 import { store } from './store.js'
 import './style.css'
+import VueGtag  from 'vue-gtag-next'
+// main.js
 import '@tinymomentum/liquid-glass-vue/dist/liquid-glass-vue.css'
 
-// vue-gtag をインポート
-import VueGtag from 'vue-gtag'
 
-// アプリ作成
-const app = createApp(App)
+const app = createApp(App);
 
-// vue-gtagをrouterと共に設定（GA4用）
 app.use(VueGtag, {
-  config: { id: 'G-JKFCXERMCK' }  // ← ここに **実際のGA4測定ID（G-で始まる）** を入力
-}, router)
+  property: [
+    {
+      id: 'G-JKFCXERMCK' // ← あなたのGA4測定ID
+    }
+  ]
+})
 
-app.use(store)
-app.use(router)
-app.mount('#app')
+app.use(store);
+app.use(router);
+app.mount('#app');
